96b62183849505cdcbf0c001b8dfcff884d8c40b
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-11-12 18:04:46 -0700

    Finalize code to get all quotes on the page

--- README.md
+++ README.md
@@ -2,14 +2,10 @@
 -[http://quotes.toscrape.com/](http://quotes.toscrape.com/)
 
 
-
-
 ## Pseudo-Code
 
 //go to quotes.toscrape.com
 //click on anchor for the humor tag
-//find text quotes by class
-//get text from text class with quote
+//find text quotes by class querySelectAll
+//forEach the qSA and get the inner text push it into an array
 
-
-//Remember how heroku goes to sleep? Write a script to wake your app up for you every morning.
--- scraper.js
+++ scraper.js
@@ -3,21 +3,39 @@ var nightmare = Nightmare({ show: true });
 
 //go to quotes.toscrape.com
 //click on anchor for the humor tag
-//find text quotes by class
-//get text from text class with quote
+//find text quotes by class querySelectAll
+//forEach the qSA and get the inner text push it into an array
+
 
 nightmare
   .goto('http://quotes.toscrape.com/')
   .click('a[href="/tag/humor/"]')
-  .wait('span')
+  .wait(250)
   .evaluate(() => {
-    return document.querySelectorAll('text');
-    
+    let results = []
+    let nodes = document.querySelectorAll('.text');
+    nodes.forEach((node, i) => results.push(document.querySelectorAll('.text')[i].innerText) )
+    return results
   })
   .end()
-  .then(result => {
-    console.log(result)
-  })
+  .then(result => {console.log('result: ', result) })
   .catch(error => {
     console.error('Search failed:', error)
   })
+
+
+//Gets onlt the first quote
+  // nightmare
+  // .goto('http://quotes.toscrape.com/')
+  // .click('a[href="/tag/humor/"]')
+  // .wait(500)
+  // .evaluate(() => {
+  //   return document.querySelector('span.text').innerText;
+  // })
+  // .end()
+  // .then(result => {
+  //   console.log('result: ', result)
+  // })
+  // .catch(error => {
+  //   console.error('Search failed:', error)
+  // })
